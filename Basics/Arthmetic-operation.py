7c89833058fe83b1969e31e8e9f78520bca72270
Your Name <your-email@example.com>
2024-10-24 23:32:05 +0530

    Day1

---
+++ Basics/Arthmetic-operation.py
@@ -0,0 +1,11 @@
+a =int(input("Enter the first integer: "))
+b =int(input("Enter the second integer: "))
+
+Add = a  + b
+print(Add)
+sub = a - b
+print(sub)
+Multiply = a* b
+print(Multiply)
+Divison = a/b
+print(Divison)
