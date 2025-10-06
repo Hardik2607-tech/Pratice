fc4028ac3742c876f79235e8134fe3e4663a75d1
Hardik Saxena <Hardik.120776@stu.upes.ac.in>
2025-10-06 20:10:39 +0530

    String

---
+++ String/ValidParaentheses.py
@@ -0,0 +1,21 @@
+def isValid(s: str) -> bool:
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
+
+
+# ---- Test cases ----
+print(isValid("()"))        # True
+print(isValid("()[]{}"))    # True
+print(isValid("(]"))        # False
+print(isValid("([])"))      # True
+print(isValid("([)]"))      # False
