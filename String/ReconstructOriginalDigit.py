a2bef73cb063f123a6170f99ba0df04f99f369be
Hardik Saxena <Hardik.120776@stu.upes.ac.in>
2025-11-13 22:29:32 +0530

    String

---
+++ String/ReconstructOriginalDigit.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+def originalDigits(s: str) -> str:
+    cnt = Counter(s)
+    digits = [0] * 10
+
+    # Unique-letter digits
+    digits[0] = cnt['z']   # zero
+    digits[2] = cnt['w']   # two
+    digits[4] = cnt['u']   # four
+    digits[6] = cnt['x']   # six
+    digits[8] = cnt['g']   # eight
+
+    # Overlapping letters
+    digits[3] = cnt['h'] - digits[8]                      # three
+    digits[5] = cnt['f'] - digits[4]                      # five
+    digits[7] = cnt['s'] - digits[6]                      # seven
+    digits[1] = cnt['o'] - digits[0] - digits[2] - digits[4]  # one
+    digits[9] = cnt['i'] - digits[5] - digits[6] - digits[8]  # nine
+
+    # Build output
+    return ''.join(str(d) * digits[d] for d in range(10))
+
+
+# Example tests
+print(originalDigits("owoztneoer"))  # Output: "012"
+print(originalDigits("fviefuro"))    # Output: "45"
